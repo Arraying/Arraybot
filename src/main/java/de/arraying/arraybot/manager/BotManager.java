8e0f49c90379027f73bcb86f4a09a5f8d1b8e4e9
Arraying <paul.huebner@googlemail.com>
2021-08-23 18:21:42 +0200

    (Experimental) enable chunking filter and chunk all guilds.

--- src/main/java/de/arraying/arraybot/manager/BotManager.java
+++ src/main/java/de/arraying/arraybot/manager/BotManager.java
@@ -188,7 +188,7 @@ public final class BotManager {
                 .disableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS)
                 .setToken(configuration.isBotBeta() ? configuration.getBotBetaToken() : configuration.getBotToken())
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
-                .setChunkingFilter(ChunkingFilter.NONE)
+                .setChunkingFilter(ChunkingFilter.ALL)
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .setShardsTotal(configuration.getBotShards())
                 .addEventListeners(new ReadyListener())
